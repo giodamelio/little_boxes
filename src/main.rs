71d4b48474ef18791524b46f2532af8565224fd1
Gio d'Amelio <giodamelio@gmail.com>
2015-06-16 07:07:04 -0700

    Make box into a struct

--- src/main.rs
+++ src/main.rs
@@ -28,6 +28,11 @@ struct Args {
     flag_version: bool,
 }
 
+struct Box {
+    content: Vec<String>,
+    charset: Charset,
+}
+
 struct Charset {
     horizontal: char,
     vertical: char,
@@ -37,6 +42,46 @@ struct Charset {
     corner_down_right: char,
     t_right: char,
     t_left: char,
+}
+
+impl Box {
+    fn print(&self) {
+        //  Get the longest line in the output
+        // Cleaner approace, but max_by is still marked unstable
+        // let longest_line = match input.iter().max_by(|x| x.len()) {
+        //     Some(line) => line.len(),
+        //     _          => 0,
+        // };
+        let mut sorted_input = self.content.clone();
+        sorted_input.sort_by(|a, b| b.len().cmp(&a.len()));
+        let max_length = sorted_input[0].len();
+
+        // Print top of box
+        print!("{}", self.charset.corner_up_left);
+        for _ in 0..(max_length + 2) {
+            print!("{}", self.charset.horizontal)
+        }
+        println!("{}", self.charset.corner_up_right);
+
+        // Print the lines
+        for line in self.content.iter() {
+            print!("{} {}", self.charset.vertical, line);
+
+            // Pad shorter lines with spaces
+            for _ in 0..(max_length - line.len()) {
+                print!(" ");
+            }
+
+            println!(" {}", self.charset.vertical);
+        }
+
+        // Print bottom of box
+        print!("{}", self.charset.corner_down_left);
+        for _ in 0..(max_length + 2) {
+            print!("{}", self.charset.horizontal)
+        }
+        println!("{}", self.charset.corner_down_right);
+    }
 }
 
 fn main() {
@@ -75,11 +120,6 @@ fn main() {
             .collect();
     }
 
-    // Print it in a box
-    box_it(input);
-}
-
-fn box_it(input: Vec<String>) {
     // Defalt charset
     let charset = Charset {
         horizontal: '━',
@@ -92,39 +132,9 @@ fn box_it(input: Vec<String>) {
         t_left: '┫',
     };
 
-    //  Get the longest line in the output
-    // Cleaner approace, but max_by is still marked unstable
-    // let longest_line = match input.iter().max_by(|x| x.len()) {
-    //     Some(line) => line.len(),
-    //     _          => 0,
-    // };
-    let mut sorted_input = input.clone();
-    sorted_input.sort_by(|a, b| b.len().cmp(&a.len()));
-    let max_length = sorted_input[0].len();
-
-    // Print top of box
-    print!("{}", charset.corner_up_left);
-    for _ in 0..(max_length + 2) {
-        print!("{}", charset.horizontal)
-    }
-    println!("{}", charset.corner_up_right);
-
-    // Print the lines
-    for line in input {
-        print!("{} {}", charset.vertical, line);
-
-        // Pad shorter lines with spaces
-        for _ in 0..(max_length - line.len()) {
-            print!(" ");
-        }
-
-        println!(" {}", charset.vertical);
-    }
-
-    // Print bottom of box
-    print!("{}", charset.corner_down_left);
-    for _ in 0..(max_length + 2) {
-        print!("{}", charset.horizontal)
-    }
-    println!("{}", charset.corner_down_right);
+    let basic_box = Box {
+        content: input,
+        charset: charset,
+    };
+    basic_box.print();
 }
