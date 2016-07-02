d596858c838b16376e2c76f95074544bb6230a44
Gio d'Amelio <giodamelio@gmail.com>
2016-07-01 20:26:58 -0700

    Add shorthad for version option

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,7 @@ Options:
   -c, --charset <charset>    The charset to draw the box with [default: thick]
   -t, --title <title>        Add a title to the box
   -h, --help                 Shows this help
-  --version                  Show version
+  -v, --version                  Show version
 ";
 
 // Get the version from cargo
